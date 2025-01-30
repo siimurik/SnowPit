04f5c6de19023d94471c5ee4f40a6ff564a2428b
siimurik <82040815+siimurik@users.noreply.github.com>
2025-01-30 15:26:41 +0200

    Add files via upload

--- Pandas_SnowStorage/CSVtoPARQUET.py
+++ Pandas_SnowStorage/CSVtoPARQUET.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import chardet
 
-input_filename = 'Surface_Melt_Rate_Data.csv'
+input_filename = 'Tsi_Tso_Data.csv'
 #with open('C:\\Users\\sipuga\\Documents\\SnowStorageSolvers\\Python_based\\Snow_Storage_Data.csv', 'rb') as f:
 with open(input_filename, 'rb') as f:
     result = chardet.detect(f.read())
@@ -13,7 +13,7 @@ print(encoding)
 df = pd.read_csv(input_filename, skiprows=1, encoding=encoding)
 
 # Convert the DataFrame to a Parquet file
-output_filename = 'SurfaceMeltRateDATA.parquet'
+output_filename = 'TsiTsoDATA.parquet'
 df.to_parquet(output_filename)    # PyArrow and fastparquet are necessary
 
 print(f"CSV data has been successfully converted to Parquet format and saved as '{output_filename}'.")
